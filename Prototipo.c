735d79785a605748e6338190880dd06583b3572c
victor-joao-f <73721481+victor-joao-f@users.noreply.github.com>
2020-10-30 19:46:04 -0300

    Add files via upload

---
+++ Prototipo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+typedef struct _JOGADOR{
+    char[42] nome;
+    int pontos;
+    
+};
+
+typedef struct _PERFIL{
+    char pergunta1[];
+    char pergunta2[];
+    char pergunta3[];
+    char pergunta4[];
+    char pergunta5[];
+    char resposta;
+    char tipo[];
+    int jaUsado =0;
+}
+
+/*
+   Primeiro a gente tem q criar um banco de dados com os perfis e as perguntas que a gente quer, depois fazer um sistema onde, na sua vez, cada jogador recebe um "perfil" aleatorio
+do banco de dados. Ali ele tem a opçao de pedir uma dica (qnd o gets captar "dica") ou dar um palpite. se o palpite for igual ao "char resposta" do perfil analisado, ele ganha n pontos
+(6-o numero de dicas usadas). Após isso, o próximo jogador recebe um outro perfil, com "jaUsado" = 0, e repete o mesmo procedimento. O primeiro jogador a conseguir 140 pontos vence, pois
+cada ponto é equivalente ao numero de casas andadas.
+*/
+
