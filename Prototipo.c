3f5c519cc8cd9a03dde546e09e62ea9a9683c384
victor-joao-f <73721481+victor-joao-f@users.noreply.github.com>
2020-11-01 12:41:09 -0300

    Update Prototipo.c

--- Prototipo.c
+++ Prototipo.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-
+#define MAX 120
+#define TENTATIVAS 6
 
 typedef struct _JOGADOR{
     char[42] nome;
     int pontos;
+    int usavel;
     
-};
+} JOGADOR;
 
 typedef struct _PERFIL{
     char pergunta1[];
@@ -14,9 +16,9 @@ typedef struct _PERFIL{
     char pergunta4[];
     char pergunta5[];
     char resposta;
-    char tipo[];
-    int jaUsado =0;
-}
+    int jaUsado = 0;
+    int pontuacao = 6
+} PERFIL;
 
 /*
    Primeiro a gente tem q criar um banco de dados com os perfis e as perguntas que a gente quer, depois fazer um sistema onde, na sua vez, cada jogador recebe um "perfil" aleatorio
@@ -25,3 +27,17 @@ do banco de dados. Ali ele tem a opçao de pedir uma dica (qnd o gets captar "di
 cada ponto é equivalente ao numero de casas andadas.
 */
 
+int main(){
+
+    int jogadores, i, JOGADOR players[6], tamanhoMaxPerfil;
+    printf("Insira a quantidade de jogadores (2-6)\n");
+    scanf("%d", &jogadores);
+
+    for(i=0;i<jogadores;i++){
+        players[i].usavel=1;
+    }
+
+
+
+
+}
