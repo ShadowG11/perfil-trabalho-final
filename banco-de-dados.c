f6b200a128c763ec1752c38ba3d838da708566cf
victor-joao-f <73721481+victor-joao-f@users.noreply.github.com>
2020-11-03 15:42:43 -0300

    Create banco-de-dados.c

---
+++ banco-de-dados.c
@@ -0,0 +1,90 @@
+void BancoDePerfis(PERFIL cartas[]) {
+    int a = 0;
+
+    strcpy(cartas[a].pergunta[0],"Dica 1: Navegador e explorador");
+    strcpy(cartas[a].pergunta[1],"Dica 2: Nasceu em Portugal");
+    strcpy(cartas[a].pergunta[2],"Dica 3: Descobriu o Brasil");
+    strcpy(cartas[a].pergunta[3],"Dica 4: Mesmo nome de um imperador do Brasil");
+    strcpy(cartas[a].pergunta[4],"Dica 5: ");
+    strcpy(cartas[a].resposta,"pedro alvares cabral");
+    
+    a++;
+
+    strcpy(cartas[a].pergunta[0],"Dica 1: Pintor famoso");
+    strcpy(cartas[a].pergunta[1],"Dica 2: Importante figura da Renascenca");
+    strcpy(cartas[a].pergunta[2],"Dica 3: Nasceu na Italia");
+    strcpy(cartas[a].pergunta[3],"Dica 4: Fez grandes contribuicoes em varias areas");
+    strcpy(cartas[a].pergunta[4],"Dica 5: Pintou a Mona Lisa");
+    strcpy(cartas[a].resposta,"leonardo da vinci");
+    
+    a++;
+
+    strcpy(cartas[a].pergunta[0],"Dica 1: Fica no Banheiro");
+    strcpy(cartas[a].pergunta[1],"Dica 2: Feito de plastico");
+    strcpy(cartas[a].pergunta[2],"Dica 3: Usado junto com outro produto");
+    strcpy(cartas[a].pergunta[3],"Dica 4: Tem cerdas");
+    strcpy(cartas[a].pergunta[4],"Dica 5: Usado nos dentes");
+    strcpy(cartas[a].resposta,"escova de dentes");
+    
+    a++;
+
+    strcpy(cartas[a].pergunta[0],"Dica 1: ");
+    strcpy(cartas[a].pergunta[1],"Dica 2: ");
+    strcpy(cartas[a].pergunta[2],"Dica 3: ");
+    strcpy(cartas[a].pergunta[3],"Dica 4: ");
+    strcpy(cartas[a].pergunta[4],"Dica 5: ");
+    strcpy(cartas[a].resposta,"");
+    
+    a++;
+
+    strcpy(cartas[a].pergunta[0],"");
+    strcpy(cartas[a].pergunta[1],"");
+    strcpy(cartas[a].pergunta[2],"");
+    strcpy(cartas[a].pergunta[3],"");
+    strcpy(cartas[a].pergunta[4],"");
+    strcpy(cartas[a].resposta,"");
+    
+    a++;
+
+    strcpy(cartas[a].pergunta[0],"");
+    strcpy(cartas[a].pergunta[1],"");
+    strcpy(cartas[a].pergunta[2],"");
+    strcpy(cartas[a].pergunta[3],"");
+    strcpy(cartas[a].pergunta[4],"");
+    strcpy(cartas[a].resposta,"");
+    
+    a+;
+
+    strcpy(cartas[a].pergunta[0],"");
+    strcpy(cartas[a].pergunta[1],"");
+    strcpy(cartas[a].pergunta[2],"");
+    strcpy(cartas[a].pergunta[3],"");
+    strcpy(cartas[a].pergunta[4],"");
+    strcpy(cartas[a].resposta,"");
+    
+    a++;
+
+    strcpy(cartas[a].pergunta[0],"");
+    strcpy(cartas[a].pergunta[1],"");
+    strcpy(cartas[a].pergunta[2],"");
+    strcpy(cartas[a].pergunta[3],"");
+    strcpy(cartas[a].pergunta[4],"");
+    strcpy(cartas[a].resposta,"");
+    
+    a++;
+
+
+    /*  Copia e Cola daqui
+    
+    strcpy(cartas[a].pergunta[0],"Dica 1: ");
+    strcpy(cartas[a].pergunta[1],"Dica 2: ");
+    strcpy(cartas[a].pergunta[2],"Dica 3: ");
+    strcpy(cartas[a].pergunta[3],"Dica 4: ");
+    strcpy(cartas[a].pergunta[4],"Dica 5: ");
+    strcpy(cartas[a].resposta,"");
+    
+    a++;
+    
+    
+    */
+}
