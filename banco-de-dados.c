4c6cbf4605e6a4db65c374a7c90e1d9100b6c860
Gerson FMF <gersonfmfilho@hotmail.com>
2020-11-05 17:24:02 -0300

    atualiza bando de dados

--- banco-de-dados.c
+++ banco-de-dados.c
@@ -1,185 +1,141 @@
 void BancoDePerfis(PERFIL cartas[]) {
-    int a = 0;
+    strcpy(cartas[0].pergunta[0],"Dica 1: Navegador e explorador");
+    strcpy(cartas[0].pergunta[1],"Dica 2: Nasceu em Portugal");
+    strcpy(cartas[0].pergunta[2],"Dica 3: Descobriu o Brasil");
+    strcpy(cartas[0].pergunta[3],"Dica 4: Mesmo primeiro nome de um imperador do Brasil");
+    strcpy(cartas[0].pergunta[4],"Dica 5: ");
+    strcpy(cartas[0].resposta,"pedro alvares cabral");
 
-    strcpy(cartas[a].pergunta[0],"Dica 1: Navegador e explorador");
-    strcpy(cartas[a].pergunta[1],"Dica 2: Nasceu em Portugal");
-    strcpy(cartas[a].pergunta[2],"Dica 3: Descobriu o Brasil");
-    strcpy(cartas[a].pergunta[3],"Dica 4: Mesmo nome de um imperador do Brasil");
-    strcpy(cartas[a].pergunta[4],"Dica 5: ");
-    strcpy(cartas[a].resposta,"pedro alvares cabral");
-    
-    a++;
 
-    strcpy(cartas[a].pergunta[0],"Dica 1: Pintor famoso");
-    strcpy(cartas[a].pergunta[1],"Dica 2: Importante figura da Renascenca");
-    strcpy(cartas[a].pergunta[2],"Dica 3: Nasceu na Italia");
-    strcpy(cartas[a].pergunta[3],"Dica 4: Fez grandes contribuicoes em varias areas");
-    strcpy(cartas[a].pergunta[4],"Dica 5: Pintou a Mona Lisa");
-    strcpy(cartas[a].resposta,"leonardo da vinci");
-    
-    a++;
+    strcpy(cartas[1].pergunta[0],"Dica 1: Pintor famoso");
+    strcpy(cartas[1].pergunta[1],"Dica 2: Importante figura da Renascenca");
+    strcpy(cartas[1].pergunta[2],"Dica 3: Nasceu na Italia");
+    strcpy(cartas[1].pergunta[3],"Dica 4: Fez grandes contribuicoes em varias areas");
+    strcpy(cartas[1].pergunta[4],"Dica 5: Pintou a Mona Lisa");
+    strcpy(cartas[1].resposta,"leonardo da vinci");
 
-    strcpy(cartas[a].pergunta[0],"Dica 1: Fica no Banheiro");
-    strcpy(cartas[a].pergunta[1],"Dica 2: Feito de plastico");
-    strcpy(cartas[a].pergunta[2],"Dica 3: Usado junto com outro produto");
-    strcpy(cartas[a].pergunta[3],"Dica 4: Tem cerdas");
-    strcpy(cartas[a].pergunta[4],"Dica 5: Usado nos dentes");
-    strcpy(cartas[a].resposta,"escova de dentes");
-    
-    a++;
 
-    strcpy(cartas[a].pergunta[0],"Dica 1: Fisico da Suica");
-    strcpy(cartas[a].pergunta[1],"Dica 2: Nascido em 1879");
-    strcpy(cartas[a].pergunta[2],"Dica 3: Ganhou o Premio Nobel");
-    strcpy(cartas[a].pergunta[3],"Dica 4: Nascido na Alemanha");
-    strcpy(cartas[a].pergunta[4],"Dica 5: Criou a Teoria da Relatividade");
-    strcpy(cartas[a].resposta,"albert einstein");
-    
-    a++;
+    strcpy(cartas[2].pergunta[0],"Dica 1: Fica no Banheiro");
+    strcpy(cartas[2].pergunta[1],"Dica 2: Feito de plastico");
+    strcpy(cartas[2].pergunta[2],"Dica 3: Usado junto com outro produto");
+    strcpy(cartas[2].pergunta[3],"Dica 4: Tem cerdas");
+    strcpy(cartas[2].pergunta[4],"Dica 5: Usado nos dentes");
+    strcpy(cartas[2].resposta,"escova de dentes");
 
-    strcpy(cartas[a].pergunta[0],"Dica 1: Fisico da Inglaterra");
-    strcpy(cartas[a].pergunta[1],"Dica 2: Nasceu em 1743");
-    strcpy(cartas[a].pergunta[2],"Dica 3: Autodidata");
-    strcpy(cartas[a].pergunta[3],"Dica 4: Criou a Mecanica Classica");
-    strcpy(cartas[a].pergunta[4],"Dica 5: Uma maca caiu em sua cabeca");
-    strcpy(cartas[a].resposta,"isaac newton");
-    
-    a++;
 
-    strcpy(cartas[a].pergunta[0],"Dica 1: Presidente dos Estados Unidos");
-    strcpy(cartas[a].pergunta[1],"Dica 2: Nascido em 1961");
-    strcpy(cartas[a].pergunta[2],"Dica 3: Aluno de Harvard Law School");
-    strcpy(cartas[a].pergunta[3],"Dica 4: Ganhou o Nobel da Paz");
-    strcpy(cartas[a].pergunta[4],"Dica 5: Primeiro presidente negro dos EUA");
-    strcpy(cartas[a].resposta,"barack obama");
-    
-    a+;
+    strcpy(cartas[3].pergunta[0],"Dica 1: Fisico da Suica");
+    strcpy(cartas[3].pergunta[1],"Dica 2: Nascido em 1879");
+    strcpy(cartas[3].pergunta[2],"Dica 3: Ganhou o Premio Nobel");
+    strcpy(cartas[3].pergunta[3],"Dica 4: Nascido na Alemanha");
+    strcpy(cartas[3].pergunta[4],"Dica 5: Criou a Teoria da Relatividade");
+    strcpy(cartas[3].resposta,"albert einstein");
 
-    strcpy(cartas[a].pergunta[0],"Dica 1: Profeta do Islamismo");
-    strcpy(cartas[a].pergunta[1],"Dica 2: Nascido em Nazare");
-    strcpy(cartas[a].pergunta[2],"Dica 3: Nasceu em uma mangedoura");
-    strcpy(cartas[a].pergunta[3],"Dica 4: Transformou agua em vinho");
-    strcpy(cartas[a].pergunta[4],"Dica 5: Foi crucificado");
-    strcpy(cartas[a].resposta,"jesus");
-    
-    a++;
 
-    strcpy(cartas[a].pergunta[0],"Dica 1: Navegador Italiano");
-    strcpy(cartas[a].pergunta[1],"Dica 2: Nascido em 1451");
-    strcpy(cartas[a].pergunta[2],"Dica 3: 'descobriu' a America");
-    strcpy(cartas[a].pergunta[3],"Dica 4: America do Sul tem um pais nomeado em sua homenagem");
-    strcpy(cartas[a].pergunta[4],"Dica 5: Primeiro Europeu a chegar no continente americano");
-    strcpy(cartas[a].resposta,"cristovao colombo");
-    
-    a++;
-    
-    strcpy(cartas[a].pergunta[0],"Dica 1: Matematico florentino");
-    strcpy(cartas[a].pergunta[1],"Dica 2: Nasceu em 1564");
-    strcpy(cartas[a].pergunta[2],"Dica 3: Muito presente na Revolucao Cientifica");
-    strcpy(cartas[a].pergunta[3],"Dica 4: Estudou Mecanica Classica");
-    strcpy(cartas[a].pergunta[4],"Dica 5: Defendia o Heliocentrismo");
-    strcpy(cartas[a].resposta,"galileu galilei");
-    
-    a++;
-    
-        
-    strcpy(cartas[a].pergunta[0],"Dica 1: Profeta do Judaismo, Cristianismo e Islamismo");
-    strcpy(cartas[a].pergunta[1],"Dica 2: Famoso por libertar os Hebreus do Egito");
-    strcpy(cartas[a].pergunta[2],"Dica 3: Presente no livro de Exodo");
-    strcpy(cartas[a].pergunta[3],"Dica 4: Abriu o Mar Vermelho");
-    strcpy(cartas[a].pergunta[4],"Dica 5: Figura que detem a autoria da Tora");
-    strcpy(cartas[a].resposta,"moises");
-    
-    a++;
-    
-        
-    strcpy(cartas[a].pergunta[0],"Dica 1: Biologo britanico");
-    strcpy(cartas[a].pergunta[1],"Dica 2: Nasceu em 1809");
-    strcpy(cartas[a].pergunta[2],"Dica 3: Publicou o livro A Origem das Especies");
-    strcpy(cartas[a].pergunta[3],"Dica 4: Cientista Agnostico");
-    strcpy(cartas[a].pergunta[4],"Dica 5: Criou a Teoria da Evolucao");
-    strcpy(cartas[a].resposta,"charles darwin");
-    
-    a++;
-    
-        
-    strcpy(cartas[a].pergunta[0],"Dica 1: Sociologo e Economista");
-    strcpy(cartas[a].pergunta[1],"Dica 2: Nasceu na Prussia, em 1818");
-    strcpy(cartas[a].pergunta[2],"Dica 3: Grande critico do Capitalismo");
-    strcpy(cartas[a].pergunta[3],"Dica 4: Atuou na Alemanha");
-    strcpy(cartas[a].pergunta[4],"Dica 5: Criou o Socialismo");
-    strcpy(cartas[a].resposta,"karl marx");
-    
-    a++;
-        
-        
-    strcpy(cartas[a].pergunta[0],"Dica 1: Lider politico e militar frances");
-    strcpy(cartas[a].pergunta[1],"Dica 2: Nasceu em 1769");
-    strcpy(cartas[a].pergunta[2],"Dica 3: Imperador dos Franceses");
-    strcpy(cartas[a].pergunta[3],"Dica 4: Conquistou grande parte da Europa");
-    strcpy(cartas[a].pergunta[4],"Dica 5: Perdeu contra a Russia, no inverno");
-    strcpy(cartas[a].resposta,"napoleao bonaparte");
-    
-    a++;    
-        
-    strcpy(cartas[a].pergunta[0],"Dica 1: Grande inventor mecanico e cientista eletrotecnico");
-    strcpy(cartas[a].pergunta[1],"Dica 2: Nasceu em 1856");
-    strcpy(cartas[a].pergunta[2],"Dica 3: revolucionou o campo do eletromagnetismo");
-    strcpy(cartas[a].pergunta[3],"Dica 4: Criou um tipo de turbina que leva o seu nome");
-    strcpy(cartas[a].pergunta[4],"Dica 5: 'ta in shockkk??'");
-    strcpy(cartas[a].resposta,"nikola tesla");
-    
-    a++;    
-        
-    strcpy(cartas[a].pergunta[0],"Dica 1: Politico Alemao");
-    strcpy(cartas[a].pergunta[1],"Dica 2: nasceu em 1889, na austria");
-    strcpy(cartas[a].pergunta[2],"Dica 3: Queria ser artista");
-    strcpy(cartas[a].pergunta[3],"Dica 4: principal instigador da Segunda Guerra Mundial");
-    strcpy(cartas[a].pergunta[4],"Dica 5: Principal figura do Partido Nazista");
-    strcpy(cartas[a].resposta,"adolf hitler");
-    
-    a++;    
-        
-    strcpy(cartas[a].pergunta[0],"Dica 1: Compositor Alemao");
-    strcpy(cartas[a].pergunta[1],"Dica 2: Fez a transicao do classicismo para o romantismo");
-    strcpy(cartas[a].pergunta[2],"Dica 3: Um dos mais importantes Compositores da musica ocidental");
-    strcpy(cartas[a].pergunta[3],"Dica 4: Compos a musica do carro do gas");
-    strcpy(cartas[a].pergunta[4],"Dica 5: Compos a famosa peca para piano 'Sonata ao Luar'");
-    strcpy(cartas[a].resposta,"beethoven");
-    
-    a++;    
-        
-    strcpy(cartas[a].pergunta[0],"Dica 1: Alcunhado 'o Magnamico'");
-    strcpy(cartas[a].pergunta[1],"Dica 2: Nascido em 1825");
-    strcpy(cartas[a].pergunta[2],"Dica 3: Imperador do Brasil");
-    strcpy(cartas[a].pergunta[3],"Dica 4: Recebeu um colegio como presente de aniversario");
-    strcpy(cartas[a].pergunta[4],"Dica 5: Ultimo Imperador Brasileiro");
-    strcpy(cartas[a].resposta,"pedro ii");
-    
-    a++;    
-        
-    strcpy(cartas[a].pergunta[0],"Dica 1: ");
-    strcpy(cartas[a].pergunta[1],"Dica 2: ");
-    strcpy(cartas[a].pergunta[2],"Dica 3: ");
-    strcpy(cartas[a].pergunta[3],"Dica 4: ");
-    strcpy(cartas[a].pergunta[4],"Dica 5: ");
-    strcpy(cartas[a].resposta,"");
-    
-    a++;
-    
+    strcpy(cartas[4].pergunta[0],"Dica 1: Fisico da Inglaterra");
+    strcpy(cartas[4].pergunta[1],"Dica 2: Nasceu em 1743");
+    strcpy(cartas[4].pergunta[2],"Dica 3: Autodidata");
+    strcpy(cartas[4].pergunta[3],"Dica 4: Criou a Mecanica Classica");
+    strcpy(cartas[4].pergunta[4],"Dica 5: Uma maca caiu em sua cabeca");
+    strcpy(cartas[4].resposta,"isaac newton");
 
-    printf("%d", a);
-    /*  Copia e Cola daqui
+
+    strcpy(cartas[5].pergunta[0],"Dica 1: Presidente dos Estados Unidos");
+    strcpy(cartas[5].pergunta[1],"Dica 2: Nascido em 1961");
+    strcpy(cartas[5].pergunta[2],"Dica 3: Aluno de Harvard Law School");
+    strcpy(cartas[5].pergunta[3],"Dica 4: Ganhou o Nobel da Paz");
+    strcpy(cartas[5].pergunta[4],"Dica 5: Primeiro presidente negro dos EUA");
+    strcpy(cartas[5].resposta,"barack obama");
+
+
+    strcpy(cartas[6].pergunta[0],"Dica 1: Profeta do Islamismo");
+    strcpy(cartas[6].pergunta[1],"Dica 2: Nascido em Nazare");
+    strcpy(cartas[6].pergunta[2],"Dica 3: Nasceu em uma mangedoura");
+    strcpy(cartas[6].pergunta[3],"Dica 4: Transformou agua em vinho");
+    strcpy(cartas[6].pergunta[4],"Dica 5: Foi crucificado");
+    strcpy(cartas[6].resposta,"jesus");
+
+
+    strcpy(cartas[7].pergunta[0],"Dica 1: Navegador Italiano");
+    strcpy(cartas[7].pergunta[1],"Dica 2: Nascido em 1451");
+    strcpy(cartas[7].pergunta[2],"Dica 3: 'descobriu' a America");
+    strcpy(cartas[7].pergunta[3],"Dica 4: America do Sul tem um pais nomeado em sua homenagem");
+    strcpy(cartas[7].pergunta[4],"Dica 5: Primeiro Europeu a chegar no continente americano");
+    strcpy(cartas[7].resposta,"cristovao colombo");
+
     
-    strcpy(cartas[a].pergunta[0],"Dica 1: ");
-    strcpy(cartas[a].pergunta[1],"Dica 2: ");
-    strcpy(cartas[a].pergunta[2],"Dica 3: ");
-    strcpy(cartas[a].pergunta[3],"Dica 4: ");
-    strcpy(cartas[a].pergunta[4],"Dica 5: ");
-    strcpy(cartas[a].resposta,"");
+    strcpy(cartas[8].pergunta[0],"Dica 1: Matematico florentino");
+    strcpy(cartas[8].pergunta[1],"Dica 2: Nasceu em 1564");
+    strcpy(cartas[8].pergunta[2],"Dica 3: Muito presente na Revolucao Cientifica");
+    strcpy(cartas[8].pergunta[3],"Dica 4: Estudou Mecanica Classica");
+    strcpy(cartas[8].pergunta[4],"Dica 5: Defendia o Heliocentrismo");
+    strcpy(cartas[8].resposta,"galileu galilei");
+
     
-    a++;
+        
+    strcpy(cartas[9].pergunta[0],"Dica 1: Profeta do Judaismo, Cristianismo e Islamismo");
+    strcpy(cartas[9].pergunta[1],"Dica 2: Famoso por libertar os Hebreus do Egito");
+    strcpy(cartas[9].pergunta[2],"Dica 3: Presente no livro de Exodo");
+    strcpy(cartas[9].pergunta[3],"Dica 4: Abriu o Mar Vermelho");
+    strcpy(cartas[9].pergunta[4],"Dica 5: Figura que detem a autoria da Tora");
+    strcpy(cartas[9].resposta,"moises");
+
     
+        
+    strcpy(cartas[10].pergunta[0],"Dica 1: Biologo britanico");
+    strcpy(cartas[10].pergunta[1],"Dica 2: Nasceu em 1809");
+    strcpy(cartas[10].pergunta[2],"Dica 3: Publicou o livro A Origem das Especies");
+    strcpy(cartas[10].pergunta[3],"Dica 4: Cientista Agnostico");
+    strcpy(cartas[10].pergunta[4],"Dica 5: Criou a Teoria da Evolucao");
+    strcpy(cartas[10].resposta,"charles darwin");
+
     
-    */
+        
+    strcpy(cartas[11].pergunta[0],"Dica 1: Sociologo e Economista");
+    strcpy(cartas[11].pergunta[1],"Dica 2: Nasceu na Prussia, em 1818");
+    strcpy(cartas[11].pergunta[2],"Dica 3: Grande critico do Capitalismo");
+    strcpy(cartas[11].pergunta[3],"Dica 4: Atuou na Alemanha");
+    strcpy(cartas[11].pergunta[4],"Dica 5: Criou o Socialismo");
+    strcpy(cartas[11].resposta,"karl marx");
+
+        
+        
+    strcpy(cartas[12].pergunta[0],"Dica 1: Lider politico e militar frances");
+    strcpy(cartas[12].pergunta[1],"Dica 2: Nasceu em 1769");
+    strcpy(cartas[12].pergunta[2],"Dica 3: Imperador dos Franceses");
+    strcpy(cartas[12].pergunta[3],"Dica 4: Conquistou grande parte da Europa");
+    strcpy(cartas[12].pergunta[4],"Dica 5: Perdeu contra a Russia, no inverno");
+    strcpy(cartas[12].resposta,"napoleao bonaparte");
+  
+        
+    strcpy(cartas[13].pergunta[0],"Dica 1: Grande inventor mecanico e cientista eletrotecnico");
+    strcpy(cartas[13].pergunta[1],"Dica 2: Nasceu em 1856");
+    strcpy(cartas[13].pergunta[2],"Dica 3: revolucionou o campo do eletromagnetismo");
+    strcpy(cartas[13].pergunta[3],"Dica 4: Criou um tipo de turbina que leva o seu nome");
+    strcpy(cartas[13].pergunta[4],"Dica 5: 'ta in shockkk?'");
+    strcpy(cartas[13].resposta,"nikola tesla");
+   
+        
+    strcpy(cartas[14].pergunta[0],"Dica 1: Politico Alemao");
+    strcpy(cartas[14].pergunta[1],"Dica 2: nasceu em 1889, na austria");
+    strcpy(cartas[14].pergunta[2],"Dica 3: Queria ser artista");
+    strcpy(cartas[14].pergunta[3],"Dica 4: principal instigador da Segunda Guerra Mundial");
+    strcpy(cartas[14].pergunta[4],"Dica 5: Principal figura do Partido Nazista");
+    strcpy(cartas[14].resposta,"adolf hitler");
+   
+        
+    strcpy(cartas[15].pergunta[0],"Dica 1: Compositor Alemao");
+    strcpy(cartas[15].pergunta[1],"Dica 2: Fez a transicao do classicismo para o romantismo");
+    strcpy(cartas[15].pergunta[2],"Dica 3: Um dos mais importantes Compositores da musica ocidental");
+    strcpy(cartas[15].pergunta[3],"Dica 4: Compos a musica do carro do gas");
+    strcpy(cartas[15].pergunta[4],"Dica 5: Compos a famosa peca para piano 'Sonata ao Luar'");
+    strcpy(cartas[15].resposta,"beethoven");
+  
+        
+    strcpy(cartas[16].pergunta[0],"Dica 1: Alcunhado 'o Magnamico'");
+    strcpy(cartas[16].pergunta[1],"Dica 2: Nascido em 1825");
+    strcpy(cartas[16].pergunta[2],"Dica 3: Imperador do Brasil");
+    strcpy(cartas[16].pergunta[3],"Dica 4: Recebeu um colegio como presente de aniversario");
+    strcpy(cartas[16].pergunta[4],"Dica 5: Ultimo Imperador Brasileiro");
+    strcpy(cartas[16].resposta,"pedro ii");
+      
 }
